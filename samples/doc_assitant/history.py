8116fa38b272d9538725381e15a1aa97f682fc19
Kamal <kamal.farahani90@gmail.com>
2024-07-01 00:02:16 +0330

    fix bug

--- samples/doc_assitant/history.py
+++ samples/doc_assitant/history.py
@@ -1,11 +1,15 @@
+import uuid
+
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.messages import trim_messages
+from langchain_core.language_models.chat_models import BaseChatModel
 
 
 class MessageHistoryStore:
     def __init__(
         self,
+        token_counter: BaseChatModel,
         store: dict[str, BaseChatMessageHistory] | None = None,
         history_token_length: int = 1000,
     ) -> None:
@@ -22,7 +26,7 @@ class MessageHistoryStore:
         self.trimmer = trim_messages(
             max_tokens=history_token_length,
             strategy="last",
-            token_counter=None,
+            token_counter=token_counter,
             include_system=True,
             allow_partial=False,
             start_on="human",
@@ -45,4 +49,31 @@ class MessageHistoryStore:
         if session_id not in self.store:
             self.store[session_id] = ChatMessageHistory()
 
-        return self.trimmer.invoke(self.store[session_id])
+        if len(self.store[session_id].messages) > 0:
+            history = ChatMessageHistory(
+                messages=self.trimmer.invoke(self.store[session_id].messages)
+            )
+        else:
+            history = self.store[session_id]
+
+        return history
+
+
+def make_session_id() -> str:
+    """
+    Returns a session id.
+
+    Returns:
+        (str): The session id.
+    """
+    return str(uuid.uuid4())
+
+
+def make_history_config() -> dict:
+    """
+    Returns a config.
+
+    Returns:
+        (dict): The config dictionary.
+    """
+    return {"configurable": {"session_id": make_session_id()}}
