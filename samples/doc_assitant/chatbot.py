30b0c6cbe136d45a5c7c1ae7126b1a62baadd2fe
Kamal <kamal.farahani90@gmail.com>
2024-07-02 20:13:54 +0330

    implement Chatbot methods

--- samples/doc_assitant/chatbot.py
+++ samples/doc_assitant/chatbot.py
@@ -1,15 +1,18 @@
 from pathlib import Path
+from collections.abc import Iterator
 
 from langchain_core.language_models.chat_models import BaseChatModel
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.runnables import RunnablePassthrough
+from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.embeddings.embeddings import Embeddings
 from langchain_core.documents.base import Document
 from langchain_chroma import Chroma
 
 from doc_assitant.history import MessageHistoryStore, make_history_config
 from doc_assitant.utils import DocumentPages, load_pdf_documents
-from doc_assitant.prompts import user_chat_prompt, summarize_prompt
+from doc_assitant.prompts import user_chat_prompt, summarize_prompt, answer_prompt
 
 
 class Chatbot:
@@ -33,10 +36,12 @@ class Chatbot:
         self.chain = user_chat_prompt | self.llm | StrOutputParser()
 
     def setup_chatbot(self) -> None:
-        history_store = MessageHistoryStore(token_counter=self.llm)
+        self.history_config = make_history_config()
+        self.history_store = MessageHistoryStore(token_counter=self.llm)
+        self.session_id = self.history_config["configurable"]["session_id"]
         self.chatbot = RunnableWithMessageHistory(
             self.chain,  # type: ignore
-            history_store,
+            self.history_store,
             input_messages_key="question",
         )
 
@@ -72,12 +77,10 @@ class Chatbot:
             Returns:
                 (Document): The summarized document.
             """
-            history_config = make_history_config()
             last_summary = "No summary yet."
             for page in doc:
                 new_summary = summarizer.invoke(
                     {"page": page.page_content, "last_summary": last_summary},
-                    config=history_config,  # type: ignore
                 )
 
                 last_summary = new_summary
@@ -90,7 +93,7 @@ class Chatbot:
 
         return [summarize(doc) for doc in documents]
 
-    def ask(self, question: str) -> str:
+    def find_relevant_documents(self, question: str) -> list[Document]:
         """
         Asks the question.
 
@@ -100,5 +103,54 @@ class Chatbot:
         Returns:
             (str): The answer.
         """
-        result = self.retriever.invoke(question)
+        return self.retriever.invoke(question)
+
+    def ask_from_document(self, question: str, document_path: Path) -> str:
+        """
+        Asks the question from the document.
+
+        Args:
+            question: The question to ask.
+            document_path: The document to ask from.
+
+        Returns:
+            (str): The answer.
+        """
+        chain = answer_prompt | self.llm | StrOutputParser()
+        pages = PyPDFLoader(str(document_path)).load()
+        current_information = "No information yet."
+        for page in pages:
+            current_information = chain.invoke(
+                {
+                    "question": question,
+                    "new_page_content": page.page_content,
+                    "current_information": current_information,
+                }
+            )
+
+        self.history_store.add_user_message_to_history(self.session_id, question)
+
+        self.history_store.add_ai_message_to_history(
+            self.session_id, current_information
+        )
+
+        return current_information
+
+    def chat(self, question: str) -> Iterator[str]:
+        """
+        Chats with the question.
+
+        Args:
+            question: The question to chat with.
+
+        Returns:
+            (str): The answer.
+        """
+        rag = {
+            "question": RunnablePassthrough(),
+            "context": self.retriever,
+        } | self.chatbot
+
+        result = rag.stream(question, config=self.history_config)
+
         return result
