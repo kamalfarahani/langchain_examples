fd68a82cd93d81006d85b9a5a8535d132fda02eb
Kamal <kamal.farahani90@gmail.com>
2024-07-14 19:47:06 +0330

    edit summarizer chain

--- samples/paper_reader/chatbot.py
+++ samples/paper_reader/chatbot.py
@@ -1,9 +1,12 @@
 from langchain_core.language_models.chat_models import BaseChatModel
 from langchain_core.embeddings.embeddings import Embeddings
+from langchain import hub
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents.stuff import StuffDocumentsChain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.summarize import load_summarize_chain
+from langchain.chains import MapReduceDocumentsChain, ReduceDocumentsChain
+from langchain.chains.llm import LLMChain
 from langchain_chroma import Chroma
 
 from paper_reader.paper import Paper, split_paper
@@ -89,9 +92,35 @@ class Chatbot:
         Returns:
             None
         """
-        self.summarizer = load_summarize_chain(
-            self.llm,
-            chain_type="map_reduce",
+        map_prompt = hub.pull("rlm/map-prompt")
+        reduce_prompt = hub.pull("rlm/reduce-prompt")
+
+        map_chain = LLMChain(
+            llm=self.llm,
+            prompt=map_prompt,
+        )
+
+        reduce_chain = LLMChain(
+            llm=self.llm,
+            prompt=reduce_prompt,
+        )
+
+        combine_documents_chain = StuffDocumentsChain(
+            llm_chain=reduce_chain,
+            document_variable_name="doc_summaries",
+        )
+
+        reduce_documents_chain = ReduceDocumentsChain(
+            combine_documents_chain=combine_documents_chain,
+            collapse_documents_chain=combine_documents_chain,
+            token_max=4000,
+        )
+
+        self.summarizer = MapReduceDocumentsChain(
+            llm_chain=map_chain,
+            reduce_documents_chain=reduce_documents_chain,
+            document_variable_name="docs",
+            return_intermediate_steps=False,
         )
 
     def ask(self, question: str) -> str:
